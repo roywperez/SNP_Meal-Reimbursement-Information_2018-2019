6ff2a401d48adb7571de9c8dc07404a77caf0a95
Roy Perez <rperez@texasagriculture.gov>
2019-02-20 12:48:22 -0600

    Added some scripting and changed the raw data.

--- Scripts/SNP_data-clean-script.R
+++ Scripts/SNP_data-clean-script.R
@@ -1,6 +1,15 @@
 library(here)
 library(tidyverse)
 
-df <- read.delim("SNP_Meal-Reimbursement-Information-Program-Year-2018-2019_2019.csv", header = TRUE, sep = ",")
-
+###Loading Data
+df <- read.delim(here("Raw-Data","SNP_Meal-Reimbursement-Information-Program-Year-2018-2019_2019.csv"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
+###convert to tibble
+df <- as_tibble(df)
+###Removing Columns
 df <- df[-c(1,2)]
+###Adding Leading zeroes
+df$CEID <- str_pad(df$CEID, 5, pad = 0)
+df$ESC <- str_pad(df$ESC, 2, pad = 0)
+df$SiteID <- str_pad(df$SiteID, 4, pad = 0)
+###strip time from the claim date
+df$ClaimDate <- as.Date(df$ClaimDate, format = "%m/%d/%Y")
